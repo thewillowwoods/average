7f88489ced5d99d1a05ac0a5aa9ee7240a0f60cc
Willow Woods <willowwoods@Willows-MacBook-Pro.local>
2022-06-23 10:48:06 -0700

    Initial commit

---
+++ average.py
@@ -0,0 +1,8 @@
+print("Please enter five numbers:")
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+num5 = int(input())
+print("The average of those numbers is: ")
+print(str((num1 + num2 + num3 + num4 + num5)/5))
